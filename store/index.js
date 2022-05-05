6257df537ad55a4466710b7b58b7a6811268c908
talion-1979 <suzukino@umich.edu>
2022-05-05 21:15:19 +0900

    元に戻しました

--- store/index.js
+++ store/index.js
@@ -4,7 +4,7 @@ const createStore = () => {
   return new Vuex.Store({
     state: () => ({
       todos: [
-        {content: 'テストお', created: '2020-04-30 17:00', state: '作業前'},
+        {content: 'テスト', created: '2020-04-30 17:00', state: '作業前'},
         {content: 'コーディング', created: '2020-04-30 16:00', state: '作業中'},
         {content: '環境構築', created: '2020-04-30 15:30', state: '完了'}
       ],
